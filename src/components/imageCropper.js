2345a5b37c299845ce4728308b5cd65feb5e1d07
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2021-06-03 16:05:44 -0700

    preview gif, cropping successful, no components

---
+++ src/components/imageCropper.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Cropper from "cropperjs"
+import "cropperjs/dist/cropper.min.css"
+import "../index.css"
+
+export default class imageCropper extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.imageElement = React.createRef();
+  }
+
+  componentDidMount() {
+    const cropper
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/components/sourceVideo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class sourceVideo extends Component {
+
+
+  
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
