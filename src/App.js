1da62686e7b49960f4c2c86dc59a2224aa333542
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2021-06-03 16:07:28 -0700

    preview gif, cropping successful, no components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
+import Cropper from "cropperjs";
 import "./App.css";
 
 // installs ffmpeg packages
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-//allows us to work w ffmpeg lib and logs eveything that (ffmpeg) it does
+//allows us to work w ffmpeg lib and logs eveything that it (ffmpeg) does
 const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
   // to track loading state, property "ready" w default value of false which will be flipped once the data has been loaded
   const [ready, setReady] = useState(false);
-
-  const [video, setVideo] = useState();
-
+  const [preview, setPreview] = useState();
   const [gif, setGif] = useState();
 
   const load = async () => {
@@ -24,41 +23,18 @@ function App() {
     load();
   }, []);
 
-  const previewVideo = async (vid) => {
+  const generateGif = async (size, callback, vid, croppit) => {
     //Write the video file to memory
-    ffmpeg.FS("writeFile", "input.mov", await fetchFile(vid));
-
-    //Run the FFMpeg command
+    ffmpeg.FS("writeFile", "test.mp4", await fetchFile(vid));
     await ffmpeg.run(
       "-i", //flag for input file
-      "input.mov", //input file
-      "output.mp4" //name of file written to memory
-    );
-
-    //Read the result
-    const data = ffmpeg.FS("readFile", "output.mp4");
-
-    //create a URL that the browser can reference
-    const url = URL.createObjectURL(
-      new Blob([data.buffer], { type: "video/mp4" })
-    );
-    setVideo(url);
-  };
-
-  const convertToGif = async () => {
-    //Write the video file to memory
-    ffmpeg.FS("writeFile", "test.mov", await fetchFile(video));
-
-    //Run the FFMpeg command (this is the syntax we will need to use)
-    await ffmpeg.run(
-      "-i", //flag for input file
-      "test.mov", //input file
+      "test.mp4", //input file
+      "-framerate", //framerate flag
+      "1/24",
       "-t", // flag for time/length of the output file
       "2.5", // length of output file is set to 2.5 seconds
-      // "-ss", // flag for offsetting when we start our output file from the input video
-      // "2.0", // the output gif will start from 2 seconds into the input video
       "-vf",
-      "scale=-1:360",
+      `fps=12,scale=-1:${size}${croppit ? `,crop=${size * .9}:${size * .9}:0:0` : ""}`,
       "-f", // flag for file type
       "gif", // output file type is set to gif
       "out.gif" //name of file written to memory
@@ -71,31 +47,36 @@ function App() {
     const url = URL.createObjectURL(
       new Blob([data.buffer], { type: "image/gif" })
     );
-    setGif(url);
+    callback(url);
   };
 
   return ready ? (
     <div className="App">
-      {video && (
-        <video
-          controls
-          //autoplay="true"
-          width="500"
-          src={video}
-        ></video>
-      )}
+      <h3>Preview</h3>
+      <div>
+        {preview && (
+          <img
+            width="500"
+            src={preview}
+            alt="preview"
+            id="preview"
+          />
+        )}
+      </div>
       <div>
         <input
           type="file"
           onChange={(event) => {
-            setVideo(event.target.files?.item(0));
-            previewVideo(event.target.files?.item(0));
+            generateGif(360, setPreview, event.target.files?.item(0), false);
           }}
         />
       </div>
+      <button onClick={() => generateGif(60, setGif, preview, true)}>
+        Convert
+      </button>
+      <div></div>
       <h3>Result</h3>
-      <button onClick={convertToGif}>Convert</button>
-      <div>{gif && <img src={gif} />}</div>
+      <div>{gif && <img src={gif} alt="output_gif" />}</div>
     </div>
   ) : (
     <p>Loading...</p>
